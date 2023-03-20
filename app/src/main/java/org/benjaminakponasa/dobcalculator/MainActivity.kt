0c322d66e95a2717ceea9a67b1e7a1ad8a913a21
adogbeji <baka_1993@yahoo.co.uk>
2023-03-20 16:18:34 +0000

    Create new variable for TextView

--- app/src/main/java/org/benjaminakponasa/dobcalculator/MainActivity.kt
+++ app/src/main/java/org/benjaminakponasa/dobcalculator/MainActivity.kt
@@ -4,10 +4,14 @@ import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    val tvSelectedDate: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
